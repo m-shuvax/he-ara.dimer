5717f89410a37850f6ab6068bc41e4b450587c49
m-shuvax <menachemshuvax@gmail.com>
2026-01-05 22:22:06 +0200

    c

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,6 @@ app.use(cors({ origin: "*" }));
 app.use(express.json());
 app.use(express.static('public'));
 
-// CORS - תמיכה בפרונטנד
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
