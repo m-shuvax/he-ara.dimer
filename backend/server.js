2147a00b41feee599311fdcf8f4748f811a029e9
m-shuvax <menachemshuvax@gmail.com>
2026-01-05 22:19:32 +0200

    log

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("server up on", PORT));
 
 
 app.use(express.json());
