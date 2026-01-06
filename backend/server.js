7a6f101150bc84266e81d8fdaa6943ad76479c11
m-shuvax <menachemshuvax@gmail.com>
2026-01-06 02:37:32 +0200

    fix

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log("server up on", PORT));
-import cors from "cors";
-app.use(cors({ origin: "*" }));
 
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 app.use(express.static('public'));
 
